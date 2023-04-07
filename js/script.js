fcce7dfa94f6befdfe5190eb07f38e0cb951a142
Paul Kim Set B <pkim59@my.bcit.ca>
2023-04-06 20:54:36 -0700

    light mode button complete

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const darkbutton = document.querySelector("#darkbutton")
 function themeswap() {
     document.querySelector("aside").classList.toggle("darktheme2");
     document.querySelector("main").classList.toggle("darktheme")
+    if (document.querySelector("main").classList.contains("darktheme")) {
+        darkbutton.textContent = "Light Mode"
+    }
 }   
 
 darkbutton.addEventListener("click", themeswap)
